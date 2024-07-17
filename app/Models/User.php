29d7aa5f515e572ef56a8c0c97408698883281b9
João Ribeiro <sys4soft.git@gmail.com>
2024-07-17 23:26:45 +0100

    004

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+
+    public function detail()
+    {
+        // each user has one user_details
+        return $this->hasOne(UserDetail::class);
+    }
+
+    public function department()
+    {
+        // this user belongs to a department
+        return $this->belongsTo(Department::class);
+    }
 }
--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class UserDetail extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        // each user has one user_details / each user_details belongs to a single user
+        return $this->belongsTo(User::class);
+    }
 }
