53459765f77ed80744f97ea88de4a83fdbe14253
João Ribeiro <sys4soft.git@gmail.com>
2024-07-23 22:41:58 +0100

    017

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,5 +13,8 @@ Route::middleware('auth')->group(function(){
     Route::get('/user/profile', [ProfileController::class, 'index'])->name('user.profile');
     Route::post('/user/profile/update-password', [ProfileController::class, 'updatePassword'])->name('user.profile.update-password');
     Route::post('/user/profile/update-user-data', [ProfileController::class, 'updateUserData'])->name('user.profile.update-user-data');
+
+    // department route
+    Route::get('/departments', [DepartmentController::class, 'index'])->name('departments');
 
 });
