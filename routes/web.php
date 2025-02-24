24b4486c8d5c83db8d422fa387c1b6b4b2e9fd7a
ageu_dev19 <ageumenezes423@gmail.com>
2025-02-24 17:27:22 -0300

    Start project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     echo "RH MANGNT";
+});
+
+Route::get('/email', function () {
+    Mail::raw('Test message of the HR Management', function (Message $message) {
+        $message->to('test@gmail.com')
+        ->subject('Welcome to the HR Management')
+        ->from('hr@management.com');
+    });
+
+    echo 'Email sent successfully';
 });
